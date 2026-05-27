1b777a5797682ebe5d2348238238ca76847b488b
Dennis Johansson <krajdan@gmail.com>
2026-05-27 12:07:55 +0200

    ändra variabler

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-4etlbzE3zfCcJwWRAL_Q4OYT1SZDz3s",
-  authDomain: "onskelistan-2026.firebaseapp.com",
-  projectId: "onskelistan-2026",
-  storageBucket: "onskelistan-2026.firebasestorage.app",
-  messagingSenderId: "928231779837",
-  appId: "1:928231779837:web:0ee6669b74838ea24c7d94",
-  measurementId: "G-YXW1BM11R2"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
